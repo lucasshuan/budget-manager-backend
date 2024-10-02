903b4670166679f88a662b85631416440edda6e8
jeancalriz <jean@calriz.com>
2024-10-02 15:04:19 -0300

    fix: expenditure router

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import componentRouter from "./modules/component/componentRouter";
 import customerRouter from "./modules/customer/customerRouter";
 import authMiddleware from "./middlewares/auth";
 import budgetRouter from "./modules/budget/budgetRouter";
+import expenditureRouter from "./modules/expenditure/expenditureRouter";
 
 const routes = Router();
 
@@ -11,5 +12,6 @@ routes.use("/auth", authRouter);
 routes.use("/components", authMiddleware, componentRouter);
 routes.use("/customers", authMiddleware, customerRouter);
 routes.use("/budgets", authMiddleware, budgetRouter);
+routes.use("/expenditures", authMiddleware, expenditureRouter);
 
 export default routes;
