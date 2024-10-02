8fbd1963435e7bc25e369ad22bbe32bbba6b5923
jeancalriz <jean@calriz.com>
2024-10-02 13:05:10 -0300

    feat: component find by id

--- src/modules/component/componentRouter.ts
+++ src/modules/component/componentRouter.ts
@@ -5,7 +5,7 @@ import componentController from "./componentController";
 const componentRouter = Router();
 
 componentRouter.get("/", Route(componentController.listByUserId));
-componentRouter.get("/", Route(componentController.findById));
+componentRouter.get("/:id", Route(componentController.findById));
 componentRouter.post("/", Route(componentController.create));
 componentRouter.put("/:id", Route(componentController.update));
 componentRouter.delete("/:id", Route(componentController.delete));
